5e7f84808ccfc4515c726b7214ddfacd0fd33d89
stefantrenda <stefantrenda@gmail.com>
2025-01-12 18:49:58 +0100

    continue

--- app/create-story/_components/StorySubjectInput.tsx
+++ app/create-story/_components/StorySubjectInput.tsx
@@ -1,21 +1,24 @@
-import { Textarea } from "@nextui-org/input";
+import {Textarea} from "@nextui-org/react";
 import React from "react";
 
-const StorySubjectInput = () => {
+const StorySubjectInput = ({ userSelection }: any) => {
   return (
     <div>
       <label className="font-bold text-4xl text-primary">
         Subject of the story
       </label>
-      <Textarea
-        placeholder="Enter the subject of the story"
+      {/* <Textarea
+        // placeholder="Enter the subject of the story"
         size="lg"
-        classNames={{
-            input: "resize-y min-h-[230px] text-2xl p-5",
+        // classNames={{
+        //   input: "resize-y min-h-[230px] text-2xl p-5",
+        // }}
+        value={"assssss"}
+        className="mt-3 max-w-lg"
+        onChange={(e) => {
+          console.log("test");
         }}
-
-        
-        />
+      /> */}
     </div>
   );
 };
--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -1,7 +1,21 @@
+'use client';
 import React from "react";
 import StorySubjectInput from "./_components/StorySubjectInput";
 
+export interface fieldData {
+  fieldName: string;
+  fieldValue: string;
+}
+
+
+
 function CreateStoryPage() {
+
+
+  const onHandleUserSelection = (data: fieldData) => {
+    console.log(data);
+  };
+
   return (
     <div className="p-10 md:px-20 lg:px-40">
       <h2 className="font-extrabold  text-[70px] text-primary text-center">
@@ -14,7 +28,7 @@ function CreateStoryPage() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-14">
         {/* Story Subject */}
-        <StorySubjectInput />
+        {/* <StorySubjectInput userSelection={onHandleUserSelection} /> */}
 
         {/* Story Type */}
 
